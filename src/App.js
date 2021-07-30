e3f3d4cd06c051ab18ac2b73ed88a2f8a8c9fe5f
mannyficient <smilley197@gmail.com>
2021-07-30 15:33:49 -0400

    initial setup for seasons app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import { SeasonDisplay } from "./Components/SeasonDisplay";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      lat: null,
+      long: null,
+    };
+    console.log(props);
+  }
+  render() {
+    window.navigator.geolocation.getCurrentPosition(
+      (position) => console.log(position),
+      (err) => console.log(err)
+    );
+    return (
+      <div>
+        Latitude
+        <SeasonDisplay></SeasonDisplay>
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/Components/SeasonDisplay.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+export const SeasonDisplay = () => {
+  return <div></div>;
+};
